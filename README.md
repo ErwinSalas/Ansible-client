0689e0d60d30d8e52ad58ac0780d15e24a8c184c
root <root@localhost.localdomain>
2018-03-14 01:14:19 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Ansible-client
+#Ansible Client
+
+
+### Team Members
+
+    * Erwin Salas
+    * Juliana Campos
+    * Johana Ruiz
+    * Bryan Ruiz
+Project Description
+A platform for simplify the masive instalation or configuration of a servers node connection.
+
---
+++ loginController.js
@@ -0,0 +1,33 @@
+var app = angular.module('loginModule',["ngRoute"])
+    .controller('loginController', function($scope, $http) {
+
+        // modelo de datos.
+        $scope.ida = "";
+        $scope.pass = "";
+
+        /**
+         * Ejecuta el inicio de sesión.
+         */
+
+        $scope.doLogin = function () {
+                var id=$scope.ida;
+                var pass=$scope.pass;
+
+                if (id=="1" && pass=="1") {
+
+                    window.location.href = ('src/MainView.html');
+                } else {
+                    alert("Credenciales incorrectas");
+                }
+        }
+
+        /**
+         * Guarda la sesión en el almacenamiento local del navegador.
+         * @param json JSON de origen.
+         */
+        function saveSession(json) {
+            localStorage.setItem("session.token", json.session.token);
+            localStorage.setItem("session.owner", JSON.stringify(json.user));
+            console.log("Sesión guardada.");
+        }
+    });
